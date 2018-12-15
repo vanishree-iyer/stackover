eff4f2bff5fae07e31e7a584c70b433698e8d928
vanishree <vanishree@exotel.com>
2018-12-15 15:41:46 +0530

    added answer routes

--- questions.go
+++ questions.go
@@ -19,13 +19,18 @@ import (
 type Question struct {
 	Id     int    `json:"id,omitempty"`
 	Que    string `json:"question,omitempty"`
-	UserId string    `json:"userid,omitempty"`
+	UserId string `json:"userid,omitempty"`
 	Votes  int    `json:"votes,omitempty"`
 	Url    string `json:"Url,omitempty"`
 }
 type QView struct {
 	Total    int
 	Question []Question
+}
+type QAView struct {
+	Qid    int
+	Ques   string
+	Answer []Answer
 }
 type QRequest struct {
 	Que string `json:"query,omitempty"`
@@ -36,13 +41,14 @@ type Answer struct {
 	Ans    string `json:"answer"`
 	Votes  int    `json:"votes"`
 	UserId string `json:"user_id"`
+	Url    string `json:"Url,omitempty"`
 }
 type Qkv struct {
 	Key   Question
 	Value int
 }
 type Akv struct {
-	Key   Question
+	Key   Answer
 	Value int
 }
 
@@ -125,7 +131,6 @@ func GetQuestion(value string) string {
 	var qs []Question
 	for _, val := range values {
 		wq := elastic.NewWildcardQuery("question", "*"+val+"*")
-		//q := elastic.NewTermQuery("question", val)
 		result, err := client.Search().Query(wq).Index("questions").Type("_doc").Do(context.TODO())
 		if err != nil {
 
@@ -172,7 +177,7 @@ func GetQuestion(value string) string {
 	return string(b)
 }
 
-func GetQustionById(id int) []Question {
+func GetQustionById(id int) QAView {
 	client, err := elastic.NewClient()
 	if err != nil {
 		fmt.Println("Error", err)
@@ -207,59 +212,114 @@ func GetQustionById(id int) []Question {
 	for _, kv := range ss {
 		res = append(res, kv.Key)
 	}
-	return res
+	ans := GetAnswerByQid(id)
+	fmt.Println(len(res))
+	return QAView{Qid: res[0].Id, Ques: res[0].Que, Answer: ans}
 }
 
-func CreateAnswer(value, uid string, qid int) {
+func CreateAnswer(a Answer) {
 	client, err := elastic.NewClient()
 	if err != nil {
-
+		fmt.Println("Error", err)
 	}
-	q := Answer{Ans: value, UserId: uid, Votes: -1, QuesId: qid}
+	id1 := rand.Intn(10000)
+	id2 := rand.Intn(10000)
+	a.Id = id1 + id2 + a.QuesId
 	req := elastic.NewBulkIndexRequest().
 		Index("answers").
 		Type("_doc").
-		Doc(q)
+		Doc(a)
 	bResp, bErr := client.Bulk().
 		Add(req).
 		Do(context.TODO())
 	if bErr != nil {
-
+		fmt.Println("Error", err)
 	}
 	fmt.Println(bResp)
 }
-
-func GetAnswer(qid int) {
-	q := elastic.NewTermQuery("ques_id", qid)
+func GetAnswerByAID(aid int) []Answer {
+	q := elastic.NewTermQuery("ans_id", aid)
 	client, err := elastic.NewClient()
 	if err != nil {
-
+		fmt.Println("Error", err)
 	}
 	result, err := client.Search().Query(q).Index("answers").Type("_doc").Do(context.TODO())
 	if err != nil {
-
+		fmt.Println("Error", err)
 	}
-	as := make(map[string]int)
+	var as []Answer
 	for _, hit := range result.Hits.Hits {
 		var a Answer
 		err := json.Unmarshal(*hit.Source, &a)
 		if err != nil {
-
+			fmt.Println("Error", err)
 		}
-		as[a.Ans] = a.Votes
+		a.Url = BASEURL + "answers/" + strconv.Itoa(a.Id)
+		as = append(as, a)
 	}
-	//var ss []kv
-	//for k, v := range as {
-	//	ss = append(ss, kv{k, v})
-	//}
-	//sort.Slice(ss, func(i, j int) bool {
-	//	return ss[i].Value > ss[j].Value
-	//})
-	//
-	//for _, kv := range ss {
-	//	fmt.Printf("%s %d\n", kv.Key, kv.Value)
-	//}
+	var res []Answer
+	amap := make(map[Answer]int)
+	for _, val := range as {
+		amap[val] = val.Votes
+	}
+	var ss []Akv
+	for k, v := range amap {
+		ss = append(ss, Akv{k, v})
+	}
+	sort.Slice(ss, func(i, j int) bool {
+		return ss[i].Value > ss[j].Value
+	})
+	for _, kv := range ss {
+		res = append(res, kv.Key)
+	}
+	return res
 }
+func GetAnswerByQid(qid int) []Answer {
+	q := elastic.NewTermQuery("ques_id", qid)
+	client, err := elastic.NewClient()
+	if err != nil {
+		fmt.Println("Error", err)
+	}
+	result, err := client.Search().Query(q).Index("answers").Type("_doc").Do(context.TODO())
+	if err != nil {
+		fmt.Println("Error", err)
+	}
+	var as []Answer
+	for _, hit := range result.Hits.Hits {
+		var a Answer
+		err := json.Unmarshal(*hit.Source, &a)
+		if err != nil {
+			fmt.Println("Error", err)
+		}
+		a.Url = BASEURL + "answers/" + strconv.Itoa(a.Id)
+		as = append(as, a)
+	}
+	var res []Answer
+	amap := make(map[Answer]int)
+	for _, val := range as {
+		amap[val] = val.Votes
+	}
+	var ss []Akv
+	for k, v := range amap {
+		ss = append(ss, Akv{k, v})
+	}
+	sort.Slice(ss, func(i, j int) bool {
+		return ss[i].Value > ss[j].Value
+	})
+	for _, kv := range ss {
+		res = append(res, kv.Key)
+	}
+	return res
+}
+
+func createQuestion(w http.ResponseWriter, r *http.Request) {
+	w.Header().Set("Content-Type", "application/json")
+	var question Question
+	_ = json.NewDecoder(r.Body).Decode(&question)
+	CreateQuestion(question)
+	json.NewEncoder(w).Encode(question)
+}
+
 func getAllQuestions(w http.ResponseWriter, r *http.Request) {
 	q := GetAllQuestions()
 	w.Header().Set("Content-Type", "application/json")
@@ -269,40 +329,43 @@ func getQuestionById(w http.ResponseWriter, r *http.Request) {
 	params := mux.Vars(r)
 	id, _ := strconv.Atoi(params["id"])
 	q := GetQustionById(id)
-	fmt.Println(q)
 	w.Header().Set("Content-Type", "application/json")
 	json.NewEncoder(w).Encode(q)
-
-	fmt.Println(params)
 }
 
-func createQuestion(w http.ResponseWriter, r *http.Request) {
-	w.Header().Set("Content-Type", "application/json")
-	var question Question
-	_ = json.NewDecoder(r.Body).Decode(&question)
-	fmt.Println(question.Id, question.Que, question.Votes)
-	CreateQuestion(question)
-	json.NewEncoder(w).Encode(question)
-}
 func getQuestions(w http.ResponseWriter, r *http.Request) {
 	w.Header().Set("Content-Type", "application/json")
 	var question QRequest
 	_ = json.NewDecoder(r.Body).Decode(&question)
-	fmt.Println(question.Que)
 	s := GetQuestion(question.Que)
-	fmt.Println(s)
-	json.NewEncoder(w).Encode(question)
+	json.NewEncoder(w).Encode(s)
+}
+
+func createAnswer(w http.ResponseWriter, r *http.Request) {
+	w.Header().Set("Content-Type", "application/json")
+	var answer Answer
+	_ = json.NewDecoder(r.Body).Decode(&answer)
+	CreateAnswer(answer)
+	json.NewEncoder(w).Encode(answer)
+}
+
+func getAnswerByAID(w http.ResponseWriter, r *http.Request) {
+	params := mux.Vars(r)
+	id, _ := strconv.Atoi(params["id"])
+	fmt.Println(id)
+	a := GetAnswerByAID(id)
+	fmt.Println(a)
+	w.Header().Set("Content-Type", "application/json")
+	json.NewEncoder(w).Encode(a)
 }
 
 func main() {
-	//GetAnswer(1)
 	r := mux.NewRouter()
 	r.HandleFunc("/api/questions", getAllQuestions).Methods("GET")
 	r.HandleFunc("/api/questions/{id}", getQuestionById).Methods("GET")
 	r.HandleFunc("/api/questions/create", createQuestion).Methods("POST")
 	r.HandleFunc("/api/questions/search", getQuestions).Methods("POST")
-
+	r.HandleFunc("/api/answers/create", createAnswer).Methods("POST")
+	r.HandleFunc("/api/answers/{id}", getAnswerByAID).Methods("GET")
 	log.Fatal(http.ListenAndServe(":8000", r))
-	//CreateAnswer("no idea ","u1",1)
-	//GetQuestion("how are you Ram")
 }
